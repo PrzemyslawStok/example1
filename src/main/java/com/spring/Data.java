e099a3b6fad49d65c516391d7bf5e3c977d63af9
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2019-04-28 12:28:02 +0200

    cd..

---
+++ src/main/java/com/spring/Data.java
@@ -0,0 +1,25 @@
+package com.spring;
+
+public class Data {
+    Integer id;
+
+    private Double X;
+    private Double Y;
+
+    public void setX(Double x){
+        X = x;
+    }
+
+    public void setY(Double y){
+        Y = y;
+    }
+
+    public Double getX(){
+        return X;
+    }
+
+    public Double getY(){
+        return Y;
+    }
+
+}
--- src/main/java/com/spring/SimpleController.java
+++ src/main/java/com/spring/SimpleController.java
@@ -1,7 +1,10 @@
 package com.spring;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
 
 @RestController
 public class SimpleController {
@@ -10,4 +13,32 @@ public class SimpleController {
     public String zwrocNapis(){
         return "Przykładowy napis";
     }
+
+    @RequestMapping("/tekst")
+    public String napis(@RequestParam String t){
+        return "tekst: "+t;
+    }
+
+    @RequestMapping("/dodaj")
+    public String dodaj(@RequestParam Double a, @RequestParam Double b){
+
+        return "suma: "+(a+b);
+    }
+
+    @RequestMapping("/printData")
+    public Iterable<Data> printData(@RequestParam Integer no){
+        ArrayList<Data> data = new ArrayList<>();
+
+        for(int i=0;i<no;i++){
+            Data d = new Data();
+            d.setX((double)i);
+            d.setY((double)i);
+
+            data.add(d);
+        }
+
+        return data;
+    }
 }
+
+
