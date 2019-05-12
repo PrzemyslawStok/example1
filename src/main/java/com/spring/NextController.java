b1ea433f838d36177fe8205026de08b7ca32d825
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2019-05-12 14:23:17 +0200

    cd..

--- src/main/java/com/spring/NextController.java
+++ src/main/java/com/spring/NextController.java
@@ -1,15 +1,36 @@
 package com.spring;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class NextController {
+    @Autowired
+    DataRepository dataRepository;
+
     @GetMapping("/showHtml")
     public String showHtml(Model model){
 
         model.addAttribute("attr0","1234556");
         return "showHtml";
+    }
+
+    @GetMapping("/addNumbers")
+    public String addNumbers(@RequestParam Double a, @RequestParam Double b, Model model){
+        model.addAttribute("a",a);
+        model.addAttribute("b",b);
+        model.addAttribute("suma",a+b);
+
+        return "addNumbers";
+    }
+
+    @GetMapping("/printDatabaseHtml")
+    public String printDatabase(Model model){
+
+
+        return "printDatabaseHtml";
     }
 }
